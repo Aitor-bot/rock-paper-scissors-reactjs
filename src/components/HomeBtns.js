c31b7c82c92168887642e7fe48f3d5ede37035ee
Ivan <ivansantanacalle@gmail.com>
2022-04-08 10:24:18 +0200

    Estilos al botón de la página Home

--- src/components/HomeBtns.js
+++ src/components/HomeBtns.js
@@ -3,31 +3,14 @@ import React from 'react'
 import styled from 'styled-components';
 import '../App.css'
 
-const Button = styled.button`
-display: inline-block;
-background: lightgray;
-color: black;
-padding: 15px 29px;
-border-radius: 2px;
-text-transform: uppercase; 
-border: 1px solid white;
-margin: 0.5em 1em;
-cursor: pointer;
-font-size: 1em;
-
-&:hover {
-  background: blue;
-}
-`
 
 function HomeBtns() {
   return (
-    <div className='homebtns-container'>          
-          <div className='home-btns'>
-            <Button className='homeBtn' onclick="">SEARCH PLAYERS</Button>
-          </div>
+    <div className='home-btns'>
+      <button class="button">PLAY GAME!</button>
     </div>
   )
 }
 
 export default HomeBtns
+
