196bf58643672b5466ddaa9ea20de3f4f8527aaa
Ivan <ivansantanacalle@gmail.com>
2022-04-08 10:05:24 +0200

    Página home y botón responsivas

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,10 +4,9 @@ import '../App.css';
 
 function Home() {
   return (
-    <div className='home-container'>
-      <img className='background' src={require("../assets/images/homeimage.png")} alt=""/>
-      <div className='home-btns'>
-        <HomeBtns />
+    <div class="main">
+      <div class="container">
+          <HomeBtns />
       </div>
     </div>
   );
