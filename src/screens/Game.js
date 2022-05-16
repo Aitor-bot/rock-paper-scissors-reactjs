7580aa5d07ac91ac23b56fd7319b5f0d881bc275
Ivan <ivansantanacalle@gmail.com>
2022-05-17 00:34:15 +0200

    Cambios estilos game.js

--- src/screens/Game.js
+++ src/screens/Game.js
@@ -1,25 +1,19 @@
 import React from 'react'
+import { Container, Row , Col } from 'react-bootstrap'
 import WebcamPlayer from "../components/WebcamPlayer"
 import OpponentChoice from "../components/OpponentChoice"
 
 function Game() {
   return (
-    <div className='container'>
-      <div className='gamescreen'>
-        <div className='row'>
-          <div className='col-4'>
-            <WebcamPlayer />
-          </div>
-          <div className='col-4'>
-            <h1 className='vs'>vs</h1>
-          </div>
-          <div className='col-4'>
-            <OpponentChoice />
-          </div>
-        </div>
-      </div>
+    <div >
+      <Container>
+        <Row className='rows'>
+          <Col className='columns'><WebcamPlayer /></Col>
+          <Col className='columns'><h1 className='vs'>vs</h1></Col>
+          <Col className='columns'><OpponentChoice /></Col>
+        </Row>
+      </Container>
     </div>
-  )
+  );
 }
-
 export default Game
