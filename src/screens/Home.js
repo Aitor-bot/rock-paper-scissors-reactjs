bafa4bc27071e9fdc26186c147e7ab55f576f393
Ivan <ivansantanacalle@gmail.com>
2022-05-16 23:26:34 +0200

    Arreglados cambios de estilos

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Grid } from '@mui/material'; 
 import HomeBtns from '../components/HomeBtns';
 import '../App.css';
 
 function Home() {
   return (
-    <div className="main">
-      <div className="container">
+    <div className='main'>
+      <Grid
+        container
+        justifyContent="center"
+        direction="column"
+        alignItems="center"
+        style={{minHeight:"100vh"}}
+      >
+        <Grid item xs={3}>
           <HomeBtns />
-      </div>
+          <img className='home-pic' src={require('../assets/images/homeimage2.png')} />
+        </Grid>
+      </Grid>
     </div>
   );
 }
